f279d21203c37bbc5b74042232e583d28e2be681
8bhsolutions <48874658+8bhsolutions@users.noreply.github.com>
2022-11-03 12:31:16 +1100

    FIX - Notification trampoline issue for Android 12

---
+++ src/android/notification/receiver/NotificationTrampolineActivity.java
@@ -0,0 +1,48 @@
+package de.appplant.cordova.plugin.notification.receiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+
+/**
+ * Android 12 does not allow BroadcastReceiver or Services to start Activities from background.
+ * This is known as Notification Trampoline
+ * https://developer.android.com/about/versions/12/behavior-changes-12#notification-trampolines
+ *
+ * This plugin sends click actions to be handled by IntentService, which then starts an activity.
+ * This class replaces the IntentService as the base class for ClickReceiver.
+ * This way the click immediately starts an activity.
+ *
+ * Similar approach to PushHandlerActivity from push plugin
+ */
+public class NotificationTrampolineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.launchApp();
+    }
+
+    /**
+     * Launch main intent from package.
+     */
+    private void launchApp() {
+        Context context = getApplicationContext();
+        String pkgName  = context.getPackageName();
+
+        Intent intent = context
+                .getPackageManager()
+                .getLaunchIntentForPackage(pkgName);
+
+        if (intent == null)
+            return;
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_FROM_BACKGROUND);
+        context.startActivity(intent);
+    }
+}
